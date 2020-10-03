b4131907c11ba26e0eb3103cd8c928a823dc4590
shravankumar99 <72029184+shravankumar99@users.noreply.github.com>
2020-10-03 19:52:06 +0530

    Update add.sh

--- add.sh
+++ add.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -x
 
-read -p "Enter first value: " x
-read -p "Enter second value: " y
-z=$(( $x + $y ))
+read -p "Enter first value: " a
+read -p "Enter second value: " b
+z=$(( $a + $b ))
 echo $z
