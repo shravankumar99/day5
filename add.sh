c3a34c57d68e744e83ad49d6ce803365b002f081
shravankumar99 <shravankateel@gmail.com>
2020-10-03 19:55:48 +0530

    conflect

--- add.sh
+++ add.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -x
 
-read -p "Enter first value: " x
-read -p "Enter second value: " y
-z=$(( $x + $y ))
+read -p "Enter first value: " m
+read -p "Enter second value: " n
+z=$(( $m + $n ))
 echo $z
